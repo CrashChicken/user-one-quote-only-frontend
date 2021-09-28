39eba1b981db9de1710acd78938b668d00abbc56
Jan Gradič <jan@gradic.net>
2021-09-28 02:26:11 +0200

    fixed router

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
